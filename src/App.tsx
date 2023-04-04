0f2b23db6bc541634afd3376f625c50c2081e8fb
Mark Wiemer <markwiemer@outlook.com>
2023-04-03 19:19:03 -0700

    Repro bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function App() {
+  const [latex, setLatex] = React.useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={latex}
+        onChange={(e) => setLatex(e.target.value)}
+      />
+      <MathJaxContext>
+        <MathJax>{`$$${latex}$$`}</MathJax>
+      </MathJaxContext>
+    </>
   );
 }
 
